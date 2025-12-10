1fa04a5a4df596822fa9f83243af6c5e5624bf16
João Pedro Viana Rodrigues <sucksocietyctt@gmail.com>
2025-12-09 22:46:22 -0300

    fix

--- todolist/main.cpp
+++ todolist/main.cpp
@@ -6,10 +6,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    // Esta linha irá imprimir a versão do Qt no painel "Saída do Aplicativo"
     qDebug() << "Usando a versão do Qt:" << QT_VERSION_STR;
     MainWindow w;
     w.setWindowTitle("To-Do List & Pomodoro");
     w.show();
     return a.exec();
 }
+
